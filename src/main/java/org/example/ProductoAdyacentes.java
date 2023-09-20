3a6800f098be1afe7572b4bec2a7f6dffc2f0450
1Neikowo <j.silva23@ufromail.cl>
2023-09-20 12:40:17 -0300

    Creación método

--- src/main/java/org/example/ProductoAdyacentes.java
+++ src/main/java/org/example/ProductoAdyacentes.java
@@ -1,7 +1,19 @@
 package org.example;
 
+
 public class ProductoAdyacentes {
 
     public static void main(String[] args) {
+        int[] arreglo = {1, -4 , 2, 2 , 5 ,-1};
+        System.out.println(productoAdyacentes(arreglo));
+    }
+    public static int productoAdyacentes(int[] arreglo) {
+        int maximo = -1000000;
+        for (int i = 0; i < arreglo.length-1; i++) {
+            if (arreglo[i] * arreglo[i+1] > maximo) {
+                maximo = arreglo[i] * arreglo[i+1];
+            }
+        }
+        return maximo;
     }
 }
