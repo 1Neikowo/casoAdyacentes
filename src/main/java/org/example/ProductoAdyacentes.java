4b43874f742fd056b453841d648639b5f7a278a9
sebas <sebasaraki@gmail.com>
2023-09-20 09:53:01 -0300

    za

--- src/main/java/org/example/ProductoAdyacentes.java
+++ src/main/java/org/example/ProductoAdyacentes.java
@@ -1,7 +1,22 @@
 package org.example;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 public class ProductoAdyacentes {
 
-    public static void main(String[] args) {
+    public static List<Integer> generarLista() {
+        Random rand = new Random();
+        int longitud = rand.nextInt(19) + 2;
+        List<Integer> lista = new ArrayList<>();
+
+        for (int i = 0; i < longitud; i++) {
+            int valor = rand.nextInt(2001) - 1000;
+            lista.add(valor);
+        }
+
+        return lista;
     }
+
+
+
 }
