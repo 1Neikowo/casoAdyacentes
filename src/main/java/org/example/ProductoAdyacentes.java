cf8017e7e8741d068899f2321ef9fbb93c1d95eb
GersonUA <g.urrea02@ufromail.cl>
2023-09-20 09:46:02 -0300

    Creacion arreglo

--- src/main/java/org/example/ProductoAdyacentes.java
+++ src/main/java/org/example/ProductoAdyacentes.java
@@ -1,6 +1,20 @@
 package org.example;
+import java.util.Random;
 
 public class ProductoAdyacentes {
+
+
+
+        public static int[] crer_arreglo() {
+            Random r = new Random();
+            int length= new Random().nextInt(18)+2 ;
+            int [] arreglo = new int[length] ;
+            for (int i = 0; i < length; i++) {
+                arreglo[i] = r.nextInt(2000)-1000;
+            }
+            return arreglo;
+        }
+
 
     public static void main(String[] args) {
     }
