79439b1ba77838b19b6238395913a472d58f4e8b
sbeae <130028710+sbeae@users.noreply.github.com>
2023-09-22 23:51:05 -0300

    Update ProductoAdyacentesTest.java

--- src/test/java/org/example/ProductoAdyacentesTest.java
+++ src/test/java/org/example/ProductoAdyacentesTest.java
@@ -1,24 +1,52 @@
 package org.example;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class ProductoAdyacentesTest {
 
-class ProductoAdyacentesTest {
-    @BeforeEach
-    void setUp() {
+
+    @Test
+    //Prueba que la función productoAdyacentes retorne el valor correcto
+    public void testProductoAdyacentesCasoDePrueba() {
+    int[] arreglo = {1, -4, 2, 2, 5, -1};
+
+    assertEquals(10, ProductoAdyacentes.productoAdyacentes(arreglo));
     }
 
-    @org.junit.jupiter.api.Test
-    void productoAdyacentes() {
-
-        int[] array = {1000,-1000};
-        int[] array2 = ProductoAdyacentes.crearArreglo(0);
-        int[] array3 = ProductoAdyacentes.crearArreglo(21);
-        int[] array4 ={0,0,0,0};
-        assertEquals(-1000000,ProductoAdyacentes.productoAdyacentes(array));
-        assertThrows(IndexOutOfBoundsException.class , () -> ProductoAdyacentes.productoAdyacentes(array2));
-        assertThrows(IndexOutOfBoundsException.class, () -> ProductoAdyacentes.productoAdyacentes(array3));
-        assertEquals(0,ProductoAdyacentes.productoAdyacentes(array4));
+    @Test
+    //Prueba si se lanza una excepción cuando se crea un arreglo con un tamaño mayor a 20
+    public void testProductoAdyacentesExcepcionLargoMayorA20() {
+        int[] arreglo = new int[21]; // Crear un arreglo con una longitud mayor a 20
+        assertThrows(IndexOutOfBoundsException.class, () -> ProductoAdyacentes.productoAdyacentes(arreglo));
     }
+
+
+    @Test
+    // Comprueba si se lanza una excepción cuando se crea un arreglo con un tamaño menor a 2
+    public void testProductoAdyacentesExcepcion() {
+        int[] arreglo = {1};
+        assertThrows(IndexOutOfBoundsException.class, () -> ProductoAdyacentes.productoAdyacentes(arreglo));
+    }
+
+    @Test
+    //Prueba si se lanza una excepción cuando se crea un arreglo con un tamaño negativo
+    public void testCrearArregloExcepcion() {
+        assertThrows(IndexOutOfBoundsException.class, () -> ProductoAdyacentes.crearArreglo(-1));
+    }
+
+
+
+    @Test
+    //Prueba si la función validarArreglo retorna true cuando el arreglo tiene un tamaño entre 2 y 20
+    public void testValidarArreglo() {
+        int[] arreglo = {1, 2, 3};
+        assertTrue(ProductoAdyacentes.validarArreglo(arreglo));
+    }
+
+
+
+
 }
